1fcf01232225ec8943b10a8094fbae370f52bfa0
Se Hyun Park <>
2020-12-24 23:57:57 +0900

    first c

---
+++ c/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  c
+//
+//  Created by Se Hyun Park on 2020/12/18.
+//
+#include<stdio.h>
+
+int main(void){
+    int age=12;
+    printf("%d\n",age);
+    age=13;
+    printf("%d\n",age);
+    return 0;
+}
+
+     
